f57609c6747496365986161fac837235a3933d26
Thiviru <thiviru1000@gmail.com>
2025-08-02 17:49:16 +1000

    comments added ofr ease

--- code/test_install.py
+++ code/test_install.py
@@ -1,10 +1,16 @@
 import sumo_rl # type: ignore
- 
+
+# This script is used to test the installation of the sumo_rl package.
+# It creates a parallel environment with a grid network and runs a simple simulation.
+
 env = sumo_rl.parallel_env(net_file='sumo-rl/sumo_rl/nets/RESCO/grid4x4/grid4x4.net.xml',
                   route_file='sumo-rl/sumo_rl/nets/RESCO/grid4x4/grid4x4_1.rou.xml',
                   use_gui=True,
                   num_seconds=3600)
+
+# Reset the environment to start a new simulation
 observations = env.reset()
-while env.agents:
-    actions = {agent: env.action_space(agent).sample() for agent in env.agents}  # this is where you would insert your policy
-    observations, rewards, terminations, truncations, infos = env.step(actions)
+
+while env.agents: # Continue until all agents are done
+    actions = {agent: env.action_space(agent).sample() for agent in env.agents}  # Sample random actions for each agent
+    observations, rewards, terminations, truncations, infos = env.step(actions) # Step the environment with the sampled actions
