af39f14a1655f348135d5c1235be26534edf2051
Thiviru <thiviru1000@gmail.com>
2025-08-16 15:12:14 +1000

    template env config added

--- code/config.py
+++ code/config.py
@@ -1,1 +1,12 @@
 # Configurations for multi-agent environment (Pettingzoo) and model training hyperparams
+
+# ! ------- ENV CONFIG -------
+# Parameters in sumo_rl.parallel_env (Get params From Line 93 in sumo-rl/sumo_rl/environment/env.py)
+ENV_CONFIG = {
+    'net_file': "",
+    'route_file': "",
+    'out_csv_name': "",
+    'use_gui': True,
+    'num_seconds': 36000,
+    # add more .. if needed
+}
