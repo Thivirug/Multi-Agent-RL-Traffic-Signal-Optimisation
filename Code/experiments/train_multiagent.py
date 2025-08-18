6756a88a29246115c4b44706c9ba3c1725c43d8f
Thiviru <thiviru1000@gmail.com>
2025-08-18 12:02:13 +1000

    change

--- Code/experiments/train_multiagent.py
+++ Code/experiments/train_multiagent.py
@@ -1,16 +1,15 @@
 # Training logic for multi-agents 
 
 from Code.config import ENV_CONFIG, PPO_hparams, DQN_hparams
-from Code.experiments.algorithms import AlgoConfigFactory
 
 import ray
 
 class Trainer:
-    def __init__(self, algo_factory: AlgoConfigFactory):
-        self.algo_factory = algo_factory
+    def __init__(self, algo_config):
+        self.algo_config = algo_config
+        
         # init ray
 
-        # build algo
         
     # build required algo 
     
