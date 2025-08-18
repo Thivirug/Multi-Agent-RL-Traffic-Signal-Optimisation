621a507005fe5177f89e7627a6814cb6d00cf7fe
Thiviru <thiviru1000@gmail.com>
2025-08-18 11:09:11 +1000

    scaffold update

--- Code/experiments/train_multiagent.py
+++ Code/experiments/train_multiagent.py
@@ -4,24 +4,32 @@ from Code.config import ENV_CONFIG, PPO_hparams, DQN_hparams
 from Code.experiments.algorithms import RL_Algorithms
 
 import ray
-# init ray
 
-# get algo config
-
-
-
-
+class Trainer:
+    def __init__(self):
+        pass
+        # init ray
 
 
-# training loop
+    # 
 
 
 
 
 
 
+    # training loop
 
 
 
 
-# close ray
+
+
+
+
+    # close ray
+
+
+if __name__ == 'main':
+    # train
+    pass
