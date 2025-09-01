57dac48a788e038935f10e5807e5d794568d0933
Thiviru <thiviru1000@gmail.com>
2025-09-01 11:36:10 +1000

    logs path edit

--- Code/config.py
+++ Code/config.py
@@ -12,7 +12,7 @@ checkpoint_freq = 2 # checkpointing freq
 ENV_CONFIG = {
     'net_file': "sumo-rl/sumo_rl/nets/2x2grid/2x2.net.xml",
     'route_file': "sumo-rl/sumo_rl/nets/2x2grid/2x2.rou.xml",
-    'out_csv_name': os.path.abspath("Code/outputs/logs"),
+    'out_csv_name': os.path.abspath("Code/outputs/logs/logs"),
     'use_gui': False,
     'num_seconds': 36000,
     # add more .. if needed
