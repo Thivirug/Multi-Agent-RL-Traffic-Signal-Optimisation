ffbbbdca23a084552df15ae3b61300ea3cf32ca7
Thiviru <thiviru1000@gmail.com>
2025-09-03 16:45:56 +1000

    update

--- Code/initial_testing/test_install.py
+++ Code/initial_testing/test_install.py
@@ -3,8 +3,8 @@ import sumo_rl # type: ignore
 # This script is used to test the installation of the sumo_rl package.
 # It creates a parallel environment with a grid network and runs a simple simulation.
 
-env = sumo_rl.parallel_env(net_file='sumo-rl/sumo_rl/nets/2x2grid/2x2.net.xml',
-                  route_file='sumo-rl/sumo_rl/nets/2x2grid/2x2.rou.xml',
+env = sumo_rl.parallel_env(net_file='src/sumo-rl/sumo_rl/nets/2x2grid/2x2.net.xml',
+                  route_file='src/sumo-rl/sumo_rl/nets/2x2grid/2x2.rou.xml',
                   use_gui=True,
                   num_seconds=2000)
 
