2ade9a8b2422596f0f69b1d5324e675626e87dc6
Thiviru <thiviru1000@gmail.com>
2025-09-10 16:13:08 +1000

    cleaned up, added logs renaming

--- Code/experiments/train_multiagent.py
+++ Code/experiments/train_multiagent.py
@@ -11,6 +11,22 @@ import ray
 import os
 import numpy as np
 
+import re
+
+def rename_logs(iter_n: int):
+    """
+        Rename the .csv log files to include iteration number
+    """
+    logs_dir = os.abspath("Code/outputs/logs")
+    pattern = re.compile(r"(logs_conn\d+_ep\d+)\.csv")
+
+    for filename in os.listdir(logs_dir):
+        match = pattern.match(filename)
+        if match and not f"_iter{iter_n + 1}" in filename:
+            base = match.group(1)
+            new_filename = f"{base}_iter{iter_n + 1}.csv"
+            os.rename(os.path.join(logs_dir, filename), os.path.join(logs_dir, new_filename))
+
 def main():
     # # init ray
     # ray.init() 
@@ -36,75 +52,24 @@ def main():
 
     # build config
     algo = config.build()
-
-    # # training loop
-    # for i in range(n_iterations): # 1 iteration =  "train_batch_size_per_learner" timesteps
-    #     results = algo.train()
-    #     # print(f"Iteration {i}: Reward = {result['policy_reward_mean']:.5f}")
-    #     # print(type(result))
-    #     mean_return = results["env_runners"].get(
-    #                 "episode_return_mean", np.nan
-    #             )
-    #     print(f"\titer={i} R={mean_return}\n")
-
-    #     # checkpoint every freq-th iter
-    #     if i % checkpoint_freq == 0:
-    #         chkpoint_dir = os.path.abspath(f"Code/outputs/checkpoints/{algo_name}/{i}") 
-    #         os.makedirs(chkpoint_dir, exist_ok=True)
-    #         chkpoint_path = algo.save_to_path(chkpoint_dir)
-    #         print(f"Checkpoint saved to {chkpoint_path}")
-
-
-    # # ! Use Tuner
-
-    # # Stop when we've either reached 100 training iterations or reward=300
-    # stopping_criteria = {"training_iteration": 5}
-    
-    # tuner = tune.Tuner(
-    #     "PPO",
-    #     tune_config=tune.TuneConfig(
-    #         metric="episode_return_mean",
-    #         mode="max",
-    #     ),
-    #     param_space=config,
-    #     run_config=tune.RunConfig(stop=stopping_criteria),
-    # )
-    # results = tuner.fit()
     
     # training loop
     for i in range(n_iterations): # 1 iteration =  "train_batch_size_per_learner" timesteps # ! use tqdm
         algo.train()
-        # # print(f"Iteration {i}: Reward = {result['policy_reward_mean']:.5f}")
-        # # print(type(result))
-        # mean_return = results["env_runners"].get(
-        #             "episode_return_mean", np.nan
-        #         )
-        # print(f"\titer={i} R={mean_return}\n")
+
+        # rename logs
+        rename_logs(i)
     
         result = algo.evaluate()
 
-        print(f"\n\tIteration {i}: Mean Reward = {result['env_runners']['episode_return_mean']:.5f}\n")
+        print(f"\n\tIteration {i+1}: Mean Reward = {result['env_runners']['episode_return_mean']:.5f}\n")
 
         # checkpoint every freq-th iter
-        if i % checkpoint_freq == 0:
-            chkpoint_dir = os.path.abspath(f"Code/outputs/checkpoints/{algo_name}/{i}") 
+        if i+1 % checkpoint_freq == 0:
+            chkpoint_dir = os.path.abspath(f"Code/outputs/checkpoints/{algo_name}/{i+1}") 
             os.makedirs(chkpoint_dir, exist_ok=True)
             chkpoint_path = algo.save_to_path(chkpoint_dir)
             print(f"/nCheckpoint saved to {chkpoint_path}/n")
-
-    # best_result = results.get_best_result()
-
-    # pprint.pprint(best_result)
-
-    # print("\nBest performing trial's final reported metrics:\n")
-
-    # metrics_to_print = [
-    #     "episode_reward_mean",
-    #     "episode_reward_max",
-    #     "episode_reward_min",
-    #     "episode_len_mean",
-    # ]
-    # pprint.pprint({k: v for k, v in best_result.metrics.items() if k in metrics_to_print})
 
     # close ray
     algo.stop()
