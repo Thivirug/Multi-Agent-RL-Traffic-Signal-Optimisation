98721a1f6fce94e0adc46a8ee7981aa03338a3ec
Thiviru <thiviru1000@gmail.com>
2025-09-12 10:20:59 +1000

    reduced sim time

--- Code/config.py
+++ Code/config.py
@@ -114,9 +114,9 @@ ENV_CONFIG = {
     'route_file': os.path.abspath('src/sumo-rl/sumo_rl/nets/2x2grid/2x2.rou.xml'),
     'out_csv_name': os.path.abspath("Code/outputs/logs/logs"),
     'use_gui': False,
-    'num_seconds': 3600,      # Total simulation time
+    'num_seconds': 1800,      # Total simulation time
     'delta_time': 5,           # Length of a simulation step (seconds)
-    'yellow_time': 2,          # Length of yellow phase
+    'yellow_time': 3,          # Length of yellow phase
     'min_green': 5,            # Minimum green time
     'max_green': 60,           # Maximum green time
     'single_agent': False,     # Multi-agent setup
