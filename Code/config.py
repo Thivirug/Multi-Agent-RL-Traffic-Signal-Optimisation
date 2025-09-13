4588a809c3514e5297798e73a43faaf4856a5f24
Thiviru <thiviru1000@gmail.com>
2025-09-13 11:32:08 +1000

    batch size increase

--- Code/config.py
+++ Code/config.py
@@ -131,7 +131,7 @@ ENV_CONFIG = {
 # PPO Hyperparameters for CTDE
 PPO_hparams = {
     'lr':2e-5,                    # Learning rate
-    'train_batch_size_per_learner': 128,  # Batch size per learner
+    'train_batch_size_per_learner': 256,  # Batch size per learner
     'entropy_coeff': 0.1,         # Entropy coefficient for exploration
     'kl_coeff': 0.2,               # KL divergence coefficient
     'clip_param': 0.2,             # PPO clipping parameter
