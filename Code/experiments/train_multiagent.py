819582080636a23b0f8ada7dc6cd234bf3e19dbf
jake Harvey <harvey.jake.roy@gmail.com>
2025-10-01 17:03:49 +1000

    save my hardware config for dqn and sac, added functionality so that the result_<alg>.json can be seen during each checkpoint iteration, instead of waiting till very end of training

--- Code/experiments/train_multiagent.py
+++ Code/experiments/train_multiagent.py
@@ -117,8 +117,12 @@ def main() -> None:
             }
             results.append(result)
 
-    # dump to json
-    print(f"\n\t Dumping Results to {json_path}...\n")
+            # write results to json file after each checkpoint
+            with open(json_path, "w") as f:
+                json.dump(results, f, indent=2)
+
+    # final dump to json (in case no checkpoints were saved)
+    print(f"\n\t Final dump to {json_path}...\n")
     with open(json_path, "w") as f:
         json.dump(results, f, indent=2)
 
